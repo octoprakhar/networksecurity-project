586e02967d6f545684666d9f5b8e0d3aa3843c5e
octoprakhar <prakhar1122334455@gmail.com>
2025-07-31 15:46:37 +0530

    Create Model Trainer Without Tracking

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig 
 
 if __name__ == "__main__":
@@ -15,6 +16,7 @@ if __name__ == "__main__":
         dataIngestionConfig = DataIngestionConfig(trainingPipelineConfig)
         dataValidationConfig = DataValidationConfig(training_pipeline_config=trainingPipelineConfig)
         dataTransformationConfig = DataTransformationConfig(training_pipeline_config=trainingPipelineConfig)
+        modelTrainerConfig = ModelTrainerConfig(training_pipeline_config=trainingPipelineConfig)
         
         
         data_ingestion = DataIngestion(dataIngestionConfig)
@@ -44,6 +46,16 @@ if __name__ == "__main__":
 
 
         print(dataTransformationArtifact)
+
+        logging.info("Model Training initiated")
+
+        model_trainer = ModelTrainer(model_trainer_config=modelTrainerConfig,data_transformation_artifact=dataTransformationArtifact)
+        modelTrainerArtifact = model_trainer.initiate_model_trainer()
+        logging.info("Model training completed")
+        print(modelTrainerArtifact)
+
+
+
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
