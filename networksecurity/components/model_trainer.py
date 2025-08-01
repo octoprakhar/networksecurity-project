ba6ee24aa52659ee56001c4d0835e7fae84d2d6c
octoprakhar <prakhar1122334455@gmail.com>
2025-08-01 12:57:22 +0530

    Create Trainer,evaluation and MLFlow

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -25,6 +25,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier,GradientBoostingClassifier,RandomForestClassifier
 
+import mlflow
+
 
 
 class ModelTrainer:
@@ -34,6 +36,17 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+    def track_mlflow(self, best_model, classificationmetric):
+        with mlflow.start_run():
+            f1_score = classificationmetric.f1_score
+            precision_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision_score",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
         
     def train_model(self,x_train,y_train, x_test,y_test):
 
@@ -91,9 +104,13 @@ class ModelTrainer:
 
         classification_train_metric = get_classification_score(y_true=y_train, y_pred=y_train_pred)
 
-        ## Track the MLFLOW
+        ## Track train experiments with the MLFLOW
+        self.track_mlflow(best_model, classification_train_metric)
 
         classification_test_metric = get_classification_score(y_true=y_test, y_pred=y_test_pred)
+
+        ## Track test experiments with the MLFLOW
+        self.track_mlflow(best_model, classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
 
